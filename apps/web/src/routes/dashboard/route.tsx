930eb32e949664a2e258d7bd7ad0f23b910e65ec
dhatguy <odunsiolakunbi@gmail.com>
2025-06-03 06:00:31 +0100

    Add Header component to dashboard layout

--- apps/web/src/routes/dashboard/route.tsx
+++ apps/web/src/routes/dashboard/route.tsx
@@ -1,6 +1,7 @@
 import { SidebarInset, SidebarProvider } from "@repo/ui/components/sidebar";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { Footer } from "src/components/footer";
+import { Header } from "src/components/header";
 import { AppSidebar } from "../../components/app-sidebar";
 
 export const Route = createFileRoute("/dashboard")({
@@ -13,12 +14,13 @@ function RouteComponent() {
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset className="bg-[#141A2D]">
-          {/* <header className="flex h-16 shrink-0 items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1" />
-          <div className="flex items-center gap-2">
+          <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+            {/* <SidebarTrigger className="-ml-1" /> */}
+            {/* <div className="flex items-center gap-2">
             <h1 className="font-semibold text-lg">Pulse Admin</h1>
-          </div>
-        </header> */}
+          </div> */}
+            <Header />
+          </header>
           <div className="flex flex-1 flex-col gap-4 p-4">
             <Outlet />
           </div>
