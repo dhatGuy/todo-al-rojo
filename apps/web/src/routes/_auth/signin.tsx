2d2c1f1eddb7642793b866e0507c4db21b03ff2d
dhatguy <odunsiolakunbi@gmail.com>
2025-06-16 23:29:07 +0100

    Fix: Improve auth pages layout and styling
    Adds `.tanstack` to `.gitignore`. Restructures and restyles the sign-in
    and sign-up pages for better responsiveness and visual appeal on
    different screen sizes, particularly focusing on larger screens (LG and
    above). Also extracts shared styles.

--- apps/web/src/routes/_auth/signin.tsx
+++ apps/web/src/routes/_auth/signin.tsx
@@ -66,128 +66,138 @@ function RouteComponent() {
   };
 
   return (
-    <section className="bg-dark-blue min-h-svh">
-      <div className="grid min-h-svh lg:grid-cols-12">
-        <main className="flex flex-col px-0 py-0 lg:col-span-5 xl:col-span-4 overflow-y-auto pb-10">
-          <div className="flex flex-col justify-between gap-10 size-full lg:max-w-3xl">
-            <div className="bg-red-700 rounded-bl-4xl px-8 py-10">
-              <Button
-                className=""
-                size="icon"
-                variant="ghost"
-                onClick={() =>
-                  canGoBack
-                    ? router.history.back()
-                    : router.navigate({
-                        to: "/",
-                      })
-                }
-              >
-                <ArrowLeft color="white" />
-              </Button>
-
-              <h1 className="mt-6 text-gray-50 text-2xl font-bold sm:text-3xl md:text-4xl">
-                Welcome <br /> Back!
-              </h1>
-            </div>
-
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                action="#"
-                className="grid grid-cols-6 gap-8 px-8"
-              >
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem className="col-span-6">
-                      <FloatingLabelInput {...field} label="Email" id="email" />
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem className="col-span-6">
-                      <FloatingLabelInput
-                        {...field}
-                        label="Password"
-                        id="password"
-                        type="password"
-                      />
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="rememberMe"
-                  render={({ field }) => (
-                    <FormItem className="col-span-6 flex items-center justify-between -mt-4">
-                      <div className="flex items-center gap-3 mb-0">
-                        <Checkbox
-                          id="rememberMe"
-                          className="bg-white data-[state=checked]:bg-white my-auto"
-                          checked={field.value}
-                          onCheckedChange={field.onChange}
-                        />
-
-                        <Label
-                          htmlFor="rememberMe"
-                          className="text-sm font-medium text-gray-200"
-                        >
-                          Remember me
-                        </Label>
-                      </div>
-                      <Button
-                        variant="link"
-                        className="font-semibold text-gray-200 p-0"
-                        type="button"
-                      >
-                        Forgot Password?
-                      </Button>
-                    </FormItem>
-                  )}
-                />
-
-                <div className="col-span-6 sm:flex flex-col sm:gap-4">
-                  <Button
-                    className="w-full rojo-gradient font-bold h-12 rounded-lg"
-                    loading={mutation.isPending}
-                  >
-                    Sign In
-                  </Button>
-                  <div className="text-center">
-                    <p className="text-gray-200 text-sm">
-                      Don't have an account?{" "}
-                      <Link
-                        to="/signup"
-                        className="text-white hover:underline font-medium"
-                      >
-                        Sign up
-                      </Link>
-                    </p>
-                  </div>
-                </div>
-              </form>
-            </Form>
-
-            <img
-              src="/todoalrojo-logo.png"
-              className="w-32 sm:w-44 mx-auto mt-8"
-            />
-          </div>
-        </main>
-
-        <aside
-          className="hidden sticky right-0 top-0 h-screen lg:block lg:col-span-7 xl:col-span-8 w-full bg-cover bg-no-repeat bg-right"
+    <section className="lg:block lg:fixed lg:inset-0 lg:z-50 bg-[#333]">
+      <div className="flex lg:items-center lg:justify-center min-h-svh lg:p-8">
+        <div
+          className={`grid lg:grid-cols-12 mx-auto w-full lg:max-w-6xl lg:rounded-4xl overflow-hidden lg:shadow-2xl lg:max-h-[90vh]
+          bg-no-repeat bg-cover
+          `}
           style={{ backgroundImage: `url(${bgSignup})` }}
-        ></aside>
+        >
+          <main className="flex flex-col px-0 py-0 lg:col-span-5 bg-dark-blue xl:col-span-4 overflow-y-auto lg:max-h-[90svh] lg:rounded-4xl">
+            <div className="flex flex-col gap-10 size-full">
+              <div className="bg-red-700 rounded-bl-4xl px-8 py-10">
+                <Button
+                  className=""
+                  size="icon"
+                  variant="ghost"
+                  onClick={() =>
+                    canGoBack
+                      ? router.history.back()
+                      : router.navigate({
+                          to: "/",
+                        })
+                  }
+                >
+                  <ArrowLeft color="white" />
+                </Button>
+
+                <h1 className="mt-6 text-gray-50 text-2xl font-bold sm:text-3xl md:text-4xl">
+                  Welcome <br /> Back!
+                </h1>
+              </div>
+
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  action="#"
+                  className="grid grid-cols-6 gap-8 px-8"
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem className="col-span-6">
+                        <FloatingLabelInput
+                          {...field}
+                          label="Email"
+                          id="email"
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem className="col-span-6">
+                        <FloatingLabelInput
+                          {...field}
+                          label="Password"
+                          id="password"
+                          type="password"
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="rememberMe"
+                    render={({ field }) => (
+                      <FormItem className="col-span-6 flex items-center justify-between -mt-4">
+                        <div className="flex items-center gap-3 mb-0">
+                          <Checkbox
+                            id="rememberMe"
+                            className="bg-white data-[state=checked]:bg-white my-auto"
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                          />
+
+                          <Label
+                            htmlFor="rememberMe"
+                            className="text-sm font-medium text-gray-200"
+                          >
+                            Remember me
+                          </Label>
+                        </div>
+                        <Button
+                          variant="link"
+                          className="font-semibold text-gray-200 p-0"
+                          type="button"
+                        >
+                          Forgot Password?
+                        </Button>
+                      </FormItem>
+                    )}
+                  />
+
+                  <div className="col-span-6 sm:flex flex-col sm:gap-4">
+                    <Button
+                      className="w-full rojo-gradient font-bold h-12 rounded-lg"
+                      loading={mutation.isPending}
+                    >
+                      Sign In
+                    </Button>
+                    <div className="text-center mt-4">
+                      <p className="text-gray-200 text-sm">
+                        Don't have an account?{" "}
+                        <Link
+                          to="/signup"
+                          className="text-white hover:underline font-medium"
+                        >
+                          Sign up
+                        </Link>
+                      </p>
+                    </div>
+                  </div>
+                </form>
+              </Form>
+
+              <div className="h-full grid items-center">
+                <img
+                  src="/todoalrojo-logo.png"
+                  className="w-32 sm:w-44 mx-auto md:mt-8"
+                />
+              </div>
+            </div>
+          </main>
+
+          <aside className="hidden sticky right-0 top-0 h-screen lg:block lg:col-span-7 xl:col-span-8 w-full"></aside>
+        </div>
       </div>
     </section>
   );
--- apps/web/src/routes/_auth/signup.tsx
+++ apps/web/src/routes/_auth/signup.tsx
@@ -93,175 +93,184 @@ function RouteComponent() {
   };
 
   return (
-    <section className="bg-dark-blue min-h-svh">
-      <div className="grid lg:min-h-screen lg:grid-cols-12">
-        <main className="flex flex-col px-0 py-0 lg:col-span-5 xl:col-span-4 overflow-y-auto pb-10">
-          <div className="flex flex-col gap-10 size-full lg:max-w-3xl">
-            <div className="bg-red-700 rounded-bl-4xl px-8 py-10">
-              <Button
-                className=""
-                size="icon"
-                variant="ghost"
-                onClick={() =>
-                  canGoBack
-                    ? router.history.back()
-                    : router.navigate({
-                        to: "/",
-                      })
-                }
-              >
-                <ArrowLeft color="white" />
-              </Button>
-
-              <h1 className="mt-6 text-gray-50 text-2xl font-bold sm:text-3xl md:text-4xl">
-                Create <br /> Account.
-              </h1>
-            </div>
-
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                action="#"
-                className="grid grid-cols-6 gap-8 px-8"
-              >
-                <FormField
-                  control={form.control}
-                  name="firstName"
-                  render={({ field }) => (
-                    <FormItem className="col-span-full">
-                      <FloatingLabelInput
-                        {...field}
-                        label="First Name"
-                        id="firstName"
-                      />
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="lastName"
-                  render={({ field }) => (
-                    <FormItem className="col-span-full">
-                      <FloatingLabelInput
-                        {...field}
-                        label="Last name"
-                        id="lastName"
-                      />
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem className="col-span-full">
-                      <FloatingLabelInput {...field} label="Email" id="email" />
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="phoneNumber"
-                  render={({ field }) => (
-                    <FormItem className="col-span-full">
-                      <FloatingLabelInput
-                        {...field}
-                        label="Phone Number"
-                        id="phoneNumber"
-                      />
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem className="col-span-full">
-                      <FloatingLabelInput
-                        {...field}
-                        label="Password"
-                        id="password"
-                        type="password"
-                      />
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="confirmPassword"
-                  render={({ field }) => (
-                    <FormItem className="col-span-full">
-                      <FloatingLabelInput
-                        {...field}
-                        label="Confirm Password"
-                        id="confirmPassword"
-                        type="password"
-                      />
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="termsAccepted"
-                  render={({ field }) => (
-                    <FormItem className="col-span-full">
-                      <div className="flex gap-3 items-center">
-                        <Checkbox
-                          className="bg-white data-[state=checked]:bg-white"
-                          id="termsAccepted"
-                          checked={field.value}
-                          onCheckedChange={field.onChange}
-                        />
-                        <FormLabel
-                          htmlFor="termsAccepted"
-                          className="text-sm text-gray-200"
-                        >
-                          Agree to terms and conditions
-                        </FormLabel>
-                      </div>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <div className="col-span-6 sm:flex flex-col sm:gap-4">
-                  <Button
-                    loading={mutation.isPending}
-                    className="w-full rojo-gradient font-bold h-12 rounded-lg"
-                  >
-                    Sign Up
-                  </Button>
-
-                  <p className="flex justify-between font-medium mt-4 text-sm text-white sm:mt-0">
-                    <span>Already have an account?</span>
-                    <Link to="/signin" className="hover:underline">
-                      Sign in
-                    </Link>
-                  </p>
-                </div>
-              </form>
-            </Form>
-
-            <img
-              src="/todoalrojo-logo.png"
-              className="w-32 sm:w-44 mx-auto mt-8"
-            />
-          </div>
-        </main>
-
-        <aside
-          className="hidden sticky right-0 top-0 h-screen lg:block lg:col-span-7 xl:col-span-8 w-full bg-cover bg-no-repeat bg-right"
+    <section className="lg:block lg:fixed lg:inset-0 lg:z-50 bg-[#333]">
+      <div className="flex lg:items-center lg:justify-center min-h-svh lg:p-8">
+        <div
+          className="grid lg:grid-cols-12 mx-auto w-full lg:max-w-6xl bg-white lg:rounded-4xl overflow-hidden lg:shadow-2xl lg:max-h-[90vh] bg-no-repeat bg-cover"
           style={{ backgroundImage: `url(${bgSignup})` }}
-        ></aside>
+        >
+          <main className="flex flex-col px-0 py-0 lg:col-span-5 bg-dark-blue xl:col-span-4 overflow-y-auto lg:max-h-[90svh] lg:rounded-4xl">
+            <div className="flex flex-col gap-10 size-full">
+              <div className="bg-red-700 rounded-bl-4xl px-8 py-10">
+                <Button
+                  className=""
+                  size="icon"
+                  variant="ghost"
+                  onClick={() =>
+                    canGoBack
+                      ? router.history.back()
+                      : router.navigate({
+                          to: "/",
+                        })
+                  }
+                >
+                  <ArrowLeft color="white" />
+                </Button>
+
+                <h1 className="mt-6 text-gray-50 text-2xl font-bold sm:text-3xl md:text-4xl">
+                  Create <br /> Account.
+                </h1>
+              </div>
+
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  action="#"
+                  className="grid grid-cols-6 gap-8 px-8"
+                >
+                  <FormField
+                    control={form.control}
+                    name="firstName"
+                    render={({ field }) => (
+                      <FormItem className="col-span-full">
+                        <FloatingLabelInput
+                          {...field}
+                          label="First Name"
+                          id="firstName"
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="lastName"
+                    render={({ field }) => (
+                      <FormItem className="col-span-full">
+                        <FloatingLabelInput
+                          {...field}
+                          label="Last name"
+                          id="lastName"
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem className="col-span-full">
+                        <FloatingLabelInput
+                          {...field}
+                          label="Email"
+                          id="email"
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="phoneNumber"
+                    render={({ field }) => (
+                      <FormItem className="col-span-full">
+                        <FloatingLabelInput
+                          {...field}
+                          label="Phone Number"
+                          id="phoneNumber"
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem className="col-span-full">
+                        <FloatingLabelInput
+                          {...field}
+                          label="Password"
+                          id="password"
+                          type="password"
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem className="col-span-full">
+                        <FloatingLabelInput
+                          {...field}
+                          label="Confirm Password"
+                          id="confirmPassword"
+                          type="password"
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="termsAccepted"
+                    render={({ field }) => (
+                      <FormItem className="col-span-full">
+                        <div className="flex gap-3 items-center">
+                          <Checkbox
+                            className="bg-white data-[state=checked]:bg-white"
+                            id="termsAccepted"
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                          />
+                          <FormLabel
+                            htmlFor="termsAccepted"
+                            className="text-sm text-gray-200"
+                          >
+                            Agree to terms and conditions
+                          </FormLabel>
+                        </div>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <div className="col-span-6 sm:flex flex-col sm:gap-4">
+                    <Button
+                      loading={mutation.isPending}
+                      className="w-full rojo-gradient font-bold h-12 rounded-lg"
+                    >
+                      Sign Up
+                    </Button>
+
+                    <p className="flex justify-between font-medium mt-4 text-sm text-white sm:mt-0">
+                      <span>Already have an account?</span>
+                      <Link to="/signin" className="hover:underline">
+                        Sign in
+                      </Link>
+                    </p>
+                  </div>
+                </form>
+              </Form>
+
+              <img
+                src="/todoalrojo-logo.png"
+                className="w-32 sm:w-44 mx-auto py-8"
+              />
+            </div>
+          </main>
+
+          <aside
+            className="hidden sticky right-0 top-0 h-screen lg:block lg:col-span-7 xl:col-span-8 w-full bg-cover bg-no-repeat bg-right"
+            style={{ backgroundImage: `url(${bgSignup})` }}
+          ></aside>
+        </div>
       </div>
     </section>
   );
