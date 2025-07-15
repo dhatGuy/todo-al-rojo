75890b805037000127fd4e80cfdf35da2cc1ae64
dhatguy <odunsiolakunbi@gmail.com>
2025-07-15 08:53:01 +0100

    clean

--- app/hooks/use-auth-user.ts
+++
@@ -1,7 +0,0 @@
-import { useRouteLoaderData } from "react-router";
-import type { loader as IndexLoader } from "~/routes/_index";
-
-export function useAuthUser() {
-	const data = useRouteLoaderData<typeof IndexLoader>("routes/_index");
-	return { ...data };
-}
